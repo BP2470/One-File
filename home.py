4ce745baa0fac8a6905a5b47f4ad08a02dd6c8e1
Brian Pham <brianp42470@gmail.com>
2022-03-21 15:38:23 -0700

    Fix formatting

--- home.py
+++ home.py
@@ -19,9 +19,9 @@ def home():
     <head>
     <title>Home page</title>
     <body>
-        <h1> Welcome {name}!
+        <h1> Welcome {name}
         <div>
-        <a href="https://www.google.com"> Not google </a>
+        <a href="https://www.google.com"> not google </a>
         </div>
         <ul>
             <li>{city_names[0]}</li>
@@ -33,4 +33,4 @@ def home():
     </html>
     '''
 
-#myobj.run()
+myobj.run()
