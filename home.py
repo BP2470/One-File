bb28c417f1f65b620beddec59090e3b5463ed7f2
Brian Pham <brianp42470@gmail.com>
2022-03-21 15:35:36 -0700

    first commit

---
+++ home.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template
+
+myobj = Flask(__name__)
+name = 'Lisa'
+city_names = ['Paris','London','Rome','Tahiti']
+'''
+        <ul>
+            {% for city in city_names}
+            <li>{{city}}</li>
+            {% endfor %}
+        </ul>
+'''
+#Links need https
+@myobj.route('/')
+def home():
+    return f'''
+    <html>
+    <head>
+    <title>Home page</title>
+    <body>
+        <h1> Welcome {name}!
+        <div>
+        <a href="https://www.google.com"> Not google </a>
+        </div>
+        <ul>
+            <li>{city_names[0]}</li>
+            <li>{city_names[1]}</li>
+            <li>{city_names[2]}</li>
+            <li>{city_names[3]}</li>
+        </ul>
+    </body>
+    </html>
+    '''
+
+#myobj.run()
